bf35d0ce71ad4ac3fda302bd4b38c88e3e308f42
kvillasenor <kvillasenor@lewisu.edu>
2026-03-28 14:56:45 -0500

    Add registration page and update login styling and validation

--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../views/login.html"));
 });
 
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "../views/register.html"));
+});
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
+
